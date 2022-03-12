be7c809c69cda395b7bdedae20f425ee75e7329a
swyner97 <swyner97@gmail.com>
2022-03-12 13:14:28 -0800

    add votes

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: "",
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -9,16 +8,20 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
-
-
-
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-app.listen(PORT, () => {
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  });
 });
